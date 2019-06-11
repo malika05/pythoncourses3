4972035193e23bc0a5ff2d34d1388ccd86610a21
Malika05 <51327247+malika05@users.noreply.github.com>
2019-06-11 16:01:13 +0200

    Add files via upload

---
+++ work6.py
@@ -0,0 +1,10 @@
+a = list()
+n = int(input())
+for i in range(n):
+    a.append(i)
+if i in range(1,21):
+    print('1-entrance')
+if i in range(21,49):
+    print('2-entrance')
+if i in range(49,91):
+    print('3-entrance')
